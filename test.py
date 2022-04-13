ffe4d586708681a14e839a0a2e9e3a6ce86aec65
rajesh094780 <37009281+rajesh094780@users.noreply.github.com>
2022-04-13 11:18:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-x = ("apple", "banana", "cherry")
-print(x)
+import requests
+import json
+
+url = "https://api.github.com/repos/rajesh094780/testGtiAction/actions/workflows/blank.yml/dispatches"
+
+payload = json.dumps({
+  "ref": "main",
+  "inputs": {
+    "id": "1234ssssssssssssssssssssss5sssssssss678"
+  }
+})
+headers = {
+  'Accept': 'application/vnd.github.everest-preview+json',
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer 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',
+  'Cookie': '_octo=GH1.1.1134568056.1649080195; logged_in=no'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
