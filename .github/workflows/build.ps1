64bb9290db58dacffd04827079d6ea3250607230
rajesh094780 <37009281+rajesh094780@users.noreply.github.com>
2022-04-22 09:12:42 +0530

    Update build.ps1

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -1,4 +1,14 @@
 param (
     $jsonObject
 )
-$jsonObject
+$hastable=$jsonObject |  ConvertFrom-Json
+$dbdetails=New-Object System.Collections.ArrayList
+$filespath=New-Object System.Collections.ArrayList
+$sourcDB=New-Object System.Collections.ArrayList
+foreach($i in $hastable.DBDetails){  
+    $dbdetails.Add($i.targetDbName)
+    $sourcDB=$i.sourceDbName
+    $filespath.Add($(Split-Path $i.targetDbPath))
+    $filespath.Add($(Split-Path $i.targetLogPath))
+}
+$filespath
